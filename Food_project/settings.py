26a7fc743fd755a66c9b19e9252690b072e9af83
Soma Kumari Bagti <somakumaribagti@gmail.com>
2025-04-19 19:59:17 +0530

    new

--- Food_project/settings.py
+++ Food_project/settings.py
@@ -26,8 +26,7 @@ SECRET_KEY = 'django-insecure-lxz_1n69msz$bf-(&m)02p*ov@-q%-p2j43*x2kxs1m1g(5x78
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = []
 
 # Application definition
 
